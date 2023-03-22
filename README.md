b539d20ac7bda27003b3697d69c747278150f27d
Mike Shen <michael.m.shen@gmail.com>
2023-03-22 23:15:06 +0000

    added files

---
+++ README.md
@@ -0,0 +1,26 @@
+# LAB - Class 03
+
+## Project: Madlibs
+
+## Author: Mike Shen
+
+## Links and Resources
+
+None
+
+## Setup
+
+None
+
+
+## How to use your library (where applicable)
+
+Not applicable
+
+## Tests
+
+To run test: pytest
+
+No test of note.  Just used the class repo supplied test file
+
+All tests passed
---
+++ madlib_cli/madlib.py
@@ -0,0 +1,91 @@
+def main():
+    # print intro
+    intro()
+
+    # Read template file
+    template_content = read_template("../assets/dark_and_stormy_night_template.txt")
+
+    # Read number of inputs, store to dict
+    stripped, parts = parse_template(template_content)
+
+    # Prompt for user input
+    words = user_input(parts)
+
+    # Generate new file with user input
+    new_story = merge(stripped, words)
+
+    save_to_file(new_story)
+
+def intro():
+    print("*"*32)
+    print("Welcome to the Mad Lib Game!")
+    print("You will be prompted to enter in some random words")
+    print("When complete, a story will be generated based on your input")
+    print("*"*32)
+    print("")
+
+def read_template(filename):
+    try:
+        with open(filename, "r") as template_file:
+            template_content = template_file.read()
+            template_content = template_content.strip()
+            return template_content
+    except FileNotFoundError:
+        raise FileNotFoundError
+
+def parse_template(template):
+    count = template.count("{")
+
+    if count == 0:
+        return "no prompts!"
+
+    temp_template = template
+    parts = []
+    stripped = ""
+
+    for x in range(count):
+        # Find index of 1st { and }
+        start = temp_template.index("{")
+        end = temp_template.index("}")
+
+        stripped = stripped + temp_template[0:start+1] + "}"
+        parts.append(temp_template[(start+1):end])
+
+        temp_template = temp_template[(end+1):]
+
+    stripped = stripped + temp_template
+
+    parts = tuple(parts)
+
+    return stripped, parts
+
+def user_input(prompt):
+    # Given a tuple with a list of prompts, return a list with user generated words
+    user_input = []
+
+    for x in prompt:
+        user_input.append(input(f"{x}: "))
+
+    return user_input
+
+def merge(stripped, words):
+    new_story = stripped
+
+    for x in words:
+        index = new_story.index("{")
+
+        # remove {}
+        new_story = new_story[0:index] + x + new_story[index+2:]
+
+    print("")
+    print(new_story)
+    print("")
+
+    return new_story
+
+def save_to_file(story):
+    with open("story.txt", 'w') as new_file:
+        new_file.write(story)
+
+if __name__ == "__main__":
+    main()
